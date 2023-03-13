d3e0d0fa7d98b07b64e520cf7e1e47a90c2ae6fe
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2023-03-13 13:26:05 +0300

    "Use permutations for validation" [#338441]

--- src/main/kotlin/ru/job4j/counter/service/ProductService.kt
+++ src/main/kotlin/ru/job4j/counter/service/ProductService.kt
@@ -8,8 +8,8 @@ import kotlin.math.roundToInt
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version 3.2
- * @since 10.03.2023
+ * @version 4.2
+ * @since 13.03.2023
  */
 @Service
 class ProductService(private val repository: ProductRepository) {
@@ -52,7 +52,41 @@ class ProductService(private val repository: ProductRepository) {
      */
     fun checkProductByName(productName: String): Boolean {
         val normalizedName = Utils.castToOneType(productName)
-        val product = repository.findByName(normalizedName)
-        return product != null
+        val words: List<String> = normalizedName.split(" ")
+        return findByNameUsingPermutations(words)
+    }
+
+    /**
+     * Метод совершает поиск продукта по базе с учетом перестановки введенных слов
+     * @param words список введенных слов
+     * @return найден ли продукт в базе
+     */
+    fun findByNameUsingPermutations(words: List<String>): Boolean {
+        var result = false
+        val factorials = IntArray(words.size + 1)
+        factorials[0] = 1
+        for (i in 1..words.size) {
+            factorials[i] = factorials[i - 1] * i
+        }
+        for (i in 0 until factorials[words.size]) {
+            var currentPermutation = ""
+            var temp = words
+            var positionCode = i
+            for (position in words.size downTo 1) {
+                val selected = positionCode / factorials[position - 1]
+                currentPermutation += temp[selected]
+                if (position > 1) {
+                    currentPermutation += " "
+                }
+                positionCode %= factorials[position - 1]
+                temp = temp.subList(0, selected) + temp.subList(selected + 1, temp.size)
+            }
+            val product = repository.findByName(currentPermutation)
+            if (product != null) {
+                result = true
+                break
+            }
+        }
+        return result
     }
 }
